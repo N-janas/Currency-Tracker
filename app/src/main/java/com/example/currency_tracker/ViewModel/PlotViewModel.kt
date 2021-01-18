998effda0bfe767dc1aa5f9484af36087157ee09
Paweł <pawelj_janusz@wp.pl>
2021-01-18 20:58:41 +0100

    Initial commit for PlotViewModel

---
+++ app/src/main/java/com/example/currency_tracker/ViewModel/PlotViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.currency_tracker.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class PlotViewModel(application: Application) : AndroidViewModel(application) {
+
+}

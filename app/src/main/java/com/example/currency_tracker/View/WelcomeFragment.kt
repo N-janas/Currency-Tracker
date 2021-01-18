ad9a0015eebe1dde480c6aa5d5075a5c94a67876
Paweł <pawelj_janusz@wp.pl>
2021-01-18 20:57:36 +0100

    Delete boilerplate fragments of code

--- app/src/main/java/com/example/currency_tracker/View/WelcomeFragment.kt
+++ app/src/main/java/com/example/currency_tracker/View/WelcomeFragment.kt
@@ -9,20 +9,7 @@ import androidx.navigation.findNavController
 import com.example.currency_tracker.R
 import kotlinx.android.synthetic.main.fragment_welcome.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class WelcomeFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,16 +26,5 @@ class WelcomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome, container, false)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WelcomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }

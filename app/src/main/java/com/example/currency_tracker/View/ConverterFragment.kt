48bd16860120535a6a336a9f3ea75c86d21b9412
Paweł <pawelj_janusz@wp.pl>
2021-01-22 22:53:04 +0100

    New functionality

--- app/src/main/java/com/example/currency_tracker/View/ConverterFragment.kt
+++ app/src/main/java/com/example/currency_tracker/View/ConverterFragment.kt
@@ -1,73 +1,125 @@
 package com.example.currency_tracker.View
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.currency_tracker.R
 import com.example.currency_tracker.ViewModel.ConverterViewModel
-import kotlinx.android.synthetic.main.fragment_converter.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.new_fragment_converter.*
+import kotlinx.android.synthetic.main.new_fragment_converter.view.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-class ConverterFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
+class ConverterFragment : Fragment(),CurrencyConverterView {
+    private var isConverted: Boolean = false
     private lateinit var converterViewModel: ConverterViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        converterViewModel = ViewModelProvider(requireActivity()).get(ConverterViewModel::class.java)
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.new_fragment_converter, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Observer for changes in result
-        val resultObserver = Observer<String>{
-            result -> tvForeignResult.text = result
-        }
+        val resultObserver = Observer<String>{ result -> view.new_converter_symbolEditText.setText(result) }
 
         // Connect result's observer
         converterViewModel.conversionResult.observe(viewLifecycleOwner, resultObserver)
 
-        // Convert currencies
-        btnConvert.setOnClickListener {
-            converterViewModel.getLatestRatesBetweenTwo(
-                sprSymbolCurrency.selectedItem.toString(),
-                sprBaseCurrency.selectedItem.toString(),
-                etBaseAmount.text.toString().toDouble()
-            ).toString()
+        val convertButton =  new_converter_convertButton as FloatingActionButton
+        val chartButton = new_converter_chartButton as FloatingActionButton
+        val favoriteButton = new_converter_favoriteButton as FloatingActionButton
+        val baseSpinner = new_converter_baseSpinner as Spinner
+        val symbolSpinner = new_converter_symbolSpinner as Spinner
+        val baseEditText = new_converter_baseEditText as EditText
+
+        convertButton.setOnClickListener {
+            convertOrSwapCurrencies()
+        }
+
+        baseSpinner.onItemSelectedListener = CurrencySpinnerAdapter(this)
+        symbolSpinner.onItemSelectedListener = CurrencySpinnerAdapter(this)
+        baseEditText.addTextChangedListener(Watcher(this))
+    }
+
+    // Convert or swap currencies
+    private fun convertOrSwapCurrencies() {
+        if(!isConverted){
+            if(isValidInput()){
+                convertAction()
+                setState(true)
+                changeButton()
+            }
+            else{
+                //TODO DING DONG WRONG
+            }
+
+        }
+        else{
+            swapCurrencies()
+            setState(false)
+            changeButton()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private fun convertAction() {
+            converterViewModel.getLatestRatesBetweenTwo(
+                    new_converter_symbolSpinner.selectedItem.toString(),
+                    new_converter_baseSpinner.selectedItem.toString(),
+                    new_converter_baseEditText.text.toString().toDouble()
+            ).toString()
 
-        converterViewModel = ViewModelProvider(requireActivity()).get(ConverterViewModel::class.java)
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_converter, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConverterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun isValidInput(): Boolean {
+        return !(new_converter_baseEditText.text.isEmpty()|| new_converter_baseEditText.text.toString().toDouble() == 0.0)
+
+    }
+
+    override fun setState(converted: Boolean) {
+        isConverted = converted
+    }
+
+    override fun changeButton() {
+        if(isConverted){
+            new_converter_convertButton.setImageResource(R.drawable.ic_baseline_repeat_24)
+        }
+        else{
+            new_converter_convertButton.setImageResource(R.drawable.ic_baseline_arrow_forward_24)
+        }
+    }
+
+    private fun swapCurrencies() {
+        val base = new_converter_baseSpinner
+        val symbol = new_converter_symbolSpinner
+
+        val baseItemPosition = base.selectedItemPosition
+        val symbolItemPosition = symbol.selectedItemPosition
+
+        base.setSelection(symbolItemPosition)
+        symbol.setSelection(baseItemPosition)
+
+        val baseValue = new_converter_baseEditText?.text
+        val symbolValue = new_converter_symbolEditText?.text
+
+        new_converter_symbolEditText.setText(baseValue.toString())
+        new_converter_baseEditText.setText(symbolValue.toString())
+
     }
 }
+
+

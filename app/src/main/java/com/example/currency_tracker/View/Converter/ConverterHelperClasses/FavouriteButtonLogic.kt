f5bd6536c9e4e2c23ba851b9e25d474e3ab7d2ba
Paweł <pawelj_janusz@wp.pl>
2021-01-30 22:03:15 +0100

    Refactoring

--- app/src/main/java/com/example/currency_tracker/View/Converter/ConverterHelperClasses/FavouriteButtonLogic.kt
+++ app/src/main/java/com/example/currency_tracker/View/Converter/ConverterHelperClasses/FavouriteButtonLogic.kt
@@ -4,16 +4,18 @@ import android.app.Activity
 import android.content.Context
 import android.util.Log
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
 import com.airbnb.lottie.LottieAnimationView
 import com.example.currency_tracker.Model.Entities.Favourites
+import com.example.currency_tracker.R
 import com.example.currency_tracker.ViewModel.FavouritesViewModel
 import kotlinx.android.synthetic.main.new_fragment_converter.view.*
 
-class FavouriteButtonLogic(private val view: View, activity: ViewModelStoreOwner) {
+class FavouriteButtonLogic(private val view: View, activity: ViewModelStoreOwner):ICurrencyFavourite {
     private val baseSpinner: Spinner = view.new_converter_baseSpinner
     private val symbolSpinner: Spinner = view.new_converter_symbolSpinner
     private val favouriteButton: LottieAnimationView = view.new_converter_favoriteButton
@@ -22,20 +24,31 @@ class FavouriteButtonLogic(private val view: View, activity: ViewModelStoreOwner
 
         val fav = Favourites(baseSpinner.selectedItemPosition,symbolSpinner.selectedItemPosition)
 
-        Log.d("Fav","Base: ${baseSpinner.selectedItemPosition} ")
-        Log.d("Fav","Symbol: ${symbolSpinner.selectedItemPosition} ")
-
         if(favouritesViewModel.alreadyNotExist(fav)){
-            Log.d("Fav","Heja")
-            favouriteButton.progress = 0f
-            favouriteButton.pauseAnimation()
             favouriteButton.playAnimation()
             favouritesViewModel.addFavourites(fav)
-            Toast.makeText(view.context,"Dodano do ulubionych",Toast.LENGTH_SHORT).show()
+
         }
         else{
             favouritesViewModel.deleteFavourites(fav)
-            Toast.makeText(view.context,"Usunięto z ulubionych",Toast.LENGTH_SHORT).show()
+            favouriteButton.playAnimation()
         }
+    }
+
+    fun checkIsOnFavouriteList() {
+        val fav = Favourites(view.new_converter_baseSpinner.selectedItemPosition,view.new_converter_symbolSpinner.selectedItemPosition)
+        if(favouritesViewModel.alreadyNotExist(fav)){
+            favouriteButton.scaleY = 1f
+            favouriteButton.progress= 0f
+        }
+        else{
+           favouriteButton.progress= 1f
+           favouriteButton.scaleY = -1f
+           favouriteButton.reverseAnimationSpeed()
+        }
+    }
+
+    override fun refreshView() {
+        checkIsOnFavouriteList()
     }
 }

90420ce791beda1dfe8c0e92c2cfddf2755a6c38
Paweł <pawelj_janusz@wp.pl>
2021-01-30 22:01:14 +0100

    Refactor, moving favourites logic into proper class

--- app/src/main/java/com/example/currency_tracker/View/Converter/ConverterFragment.kt
+++ app/src/main/java/com/example/currency_tracker/View/Converter/ConverterFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -55,8 +56,10 @@ class ConverterFragment : Fragment() {
         }
         converterOnClickListeners.initializeAllListeners()
 
+        favouriteButtonLogic.checkIsOnFavouriteList()
         view.new_converter_baseEditText.requestFocus()
     }
+
 
     private fun argumentsAreNotNull(): Boolean {
         if (args.base.isEmpty() || args.symbol.isEmpty()) {
